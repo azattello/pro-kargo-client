d0c1344bf533785a7f276afa33d76697152c5bd5
Abdykali Azat <azat.abdykalli@gmail.com>
2024-08-02 12:02:13 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://pro-kargo.kz:3001',
+    
+    nameCargo: 'Pro Kargo'
+  };
+  
+  
+export default config;
+  
